7507229d6a8128ec9972087f9b6ed96df78c581a
MarcosDemian <demiangarcia254@gmail.com>
2024-10-22 18:10:36 -0300

    New portfolio
    new portfolio

---
+++ main.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const carousel = document.querySelector(".carousel-inner");
+	const speed = 1; 
+	let scrollAmount = 0;
+	let isHovered = false;
+
+	
+	const carouselContent = carousel.innerHTML;
+	carousel.innerHTML += carouselContent;
+
+	const startScrolling = () => {
+		if (!isHovered) {
+			scrollAmount -= speed;
+			if (Math.abs(scrollAmount) >= carousel.scrollWidth / 2) {
+				scrollAmount = 0;
+			}
+			carousel.style.transform = `translateX(${scrollAmount}px)`;
+		}
+		requestAnimationFrame(startScrolling);
+	};
+
+	carousel.addEventListener("mouseover", () => {
+		isHovered = true;
+	});
+
+	carousel.addEventListener("mouseout", () => {
+		isHovered = false;
+	});
+
+	startScrolling();
+});
